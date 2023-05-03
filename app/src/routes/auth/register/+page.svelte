bee919edc872c2ad88b29ff96b22565c3dbb41dd
PUP PC <barikly@pm.me>
2023-05-03 16:47:10 +0700

    3:46 register page

---
+++ app/src/routes/auth/register/+page.svelte
@@ -0,0 +1,11 @@
+<form method="post">
+    <input type="text" name="name" placeholder="Name" required/>
+    <input type="email" name="email" placeholder="Email" required/>
+    <input type="password" name="password" placeholder="Password" required/>
+    <input type="password" name="passwordConfirm" placeholder="Confirm Password" required/>
+
+    <button type="submit">Register</button>
+    <center>
+        <a href="login">Already have an account? Login</a>
+    </center>
+</form>
